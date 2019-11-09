a7f01d8fa046ac33c84622c8844f4b99fb037d30
mohamedatfunrock <mohamed.rashad@funrock.com>
2019-11-09 15:07:17 +0200

    Add Animation Manager and handler to handle animation in game

---
+++ Assets/Scripts/Animation/AnimationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler
+{
+    public AnimationType AnimationType;
+    AnimationBehaviour animationBehaviour;
+    public GameObject RefrenceObject;
+    public Vector3 OriginalPosition;
+    public bool resetToOriginal;
+    public void Start()
+    {
+        OriginalPosition = RefrenceObject.transform.position;
+        animationBehaviour = AnimationFactory.MakeAnimation(AnimationType);
+        RefrenceObject.AddComponent(animationBehaviour.GetType());
+    }
+    public void Stop()
+    {
+        RefrenceObject.GetComponent<AnimationBehaviour>().StopAnimate();
+        if (resetToOriginal)
+        {
+            RefrenceObject.transform.position = OriginalPosition;
+        }
+    }
+    public void Reset()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Managers/AnimationManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimationType
+{
+    Shake,
+    FadeIn,
+    FadeOut,
+    ScaleIn,
+    ScaleOut,
+    None
+}
+public class AnimationManager : MonoBehaviourSingleton<AnimationManager>
+{
+    private Dictionary<GameObject, List<AnimationHandler>> AnimationList = new Dictionary<GameObject, List<AnimationHandler>>();
+    public void AddAnimation(AnimationType animationType,GameObject refrenceGameObject,bool resetToOriginal = true)
+    {
+        if (!AnimationList.ContainsKey(refrenceGameObject))
+            AnimationList.Add(refrenceGameObject, new List<AnimationHandler>());
+
+        var existsAnimationHandler = AnimationList[refrenceGameObject].Find(x => x.AnimationType == animationType);
+        if (existsAnimationHandler != null)
+            return;
+        AnimationHandler animationHandler = new AnimationHandler();
+        animationHandler.AnimationType = animationType;
+        animationHandler.RefrenceObject = refrenceGameObject;
+        animationHandler.resetToOriginal = resetToOriginal;
+        AnimationList[refrenceGameObject].Add(animationHandler);
+        animationHandler.Start();
+        
+    }
+    public void StopAnimation( GameObject refrenceGameObject, AnimationType animationType)
+    {
+        if (!AnimationList.ContainsKey(refrenceGameObject))
+            return;
+        var animationHandlerIndex = AnimationList[refrenceGameObject].FindIndex(x => x.AnimationType == animationType);
+        AnimationList[refrenceGameObject][animationHandlerIndex].Stop();
+        AnimationList[refrenceGameObject].RemoveAt(animationHandlerIndex);
+    }
+}
