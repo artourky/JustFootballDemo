69e1a9a8995df694b1b8f15a5c5391f0fae3b1f4
mohamedatfunrock <mohamed.rashad@funrock.com>
2019-11-09 15:05:42 +0200

    Add bool to allow or not allow LongPress and solve conflict netween click and longpress

--- Assets/lib/UIButton.cs
+++ Assets/lib/UIButton.cs
@@ -9,38 +9,51 @@ public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
 
     [SerializeField]
     [Tooltip("How long must pointer be down on this object to trigger a long press")]
-    private float holdTime = 1f;
+    private float holdTime = 5f;
+    private float longPressDelay = 0.5f;
 
     private bool held = false;
+    public bool AllowLongPress = false;
     public UnityEvent onClick = new UnityEvent();
-
     public UnityEvent onLongPress = new UnityEvent();
+    public UnityEvent onLongPressCanceled = new UnityEvent();
+    public UnityEvent onPointerDown = new UnityEvent();
+    public UnityEvent onLongPressStart = new UnityEvent();
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        onPointerDown.Invoke();
+        if (!AllowLongPress)
+            return;
         held = false;
-        Invoke("OnLongPress", holdTime);
+        Invoke("OnLongPress", holdTime + longPressDelay);
+        Invoke("OnLongPressStarted", longPressDelay);
     }
-
     public void OnPointerUp(PointerEventData eventData)
     {
+        if (!AllowLongPress)
+            return;
+        if (held)
+            onLongPressCanceled.Invoke();
         CancelInvoke("OnLongPress");
+        CancelInvoke("OnLongPressStarted");
     }
-
     public void OnPointerExit(PointerEventData eventData)
     {
-        CancelInvoke("OnLongPress");
     }
-
     private void OnLongPress()
     {
         held = true;
         onLongPress.Invoke();
     }
-
+    private void OnLongPressStarted()
+    {
+        held = true;
+        onLongPressStart.Invoke();
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (!held)
+        if (!held || !AllowLongPress)
             onClick.Invoke();
     }
 }
