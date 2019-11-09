0555e05600da937b45443a424aa80508ade670bc
mohamedatfunrock <mohamed.rashad@funrock.com>
2019-11-09 13:04:03 +0200

    Add Generic Singleton

---
+++ Assets/lib/MonoBehaviourSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null) _instance = (T)FindObjectOfType(typeof(T));
+            if (_instance == null)
+            {
+                var newGameObject = new GameObject(typeof(T).ToString(), typeof(T));
+                _instance = newGameObject.GetComponent<T>();
+            }
+            return _instance;
+        }
+    }
+
+    protected void Awake()
+    {
+        _instance = (T)FindObjectOfType(typeof(T));
+        if (_instance == null) _instance = this as T;
+        else if (_instance != this) Destroy(gameObject);
+    }
+}
